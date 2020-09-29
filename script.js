19d7dc1aa59f3620af86aa0ed9239b1e012d57d7
kkritii <kritiipraz@gmail.com>
2020-09-29 23:43:15 +0545

    Digital Clock

---
+++ script.js
@@ -0,0 +1,48 @@
+function showTime(){
+    var time = new Date();
+
+    var date = time.getDate();
+    var month = time.getMonth();
+    var monList = ["Jan","Feb","Mar","April","May","June","July","Aug","Sept","Oct","Nov","Dec"];
+    month = monList[month];
+    var year = time.getFullYear();
+    var day = time.getDay();
+    var dayList =["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"]; 
+    day = dayList[day];
+    day = checkTime(day);
+
+    
+
+    var h = time.getHours();
+    var min = time.getMinutes();
+    var sec = time.getSeconds();
+    var period = "AM";
+
+    if (h == 0){
+        h = 12;
+    }
+    if ( h > 12){
+        h = h-12;
+        period = "PM";
+    }
+
+    h = checkTime(h)
+    m = checkTime(min);
+    s = checkTime(sec);
+
+    document.getElementById('date').innerHTML = day + "day, " + date + " " + month + " " + year;
+
+    
+
+    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s + " " + period;
+    
+    setTimeout('showTime()',1000);
+}
+
+function checkTime(i){
+    if (i<10){
+        i = "0" + i;
+    }
+    return i;
+}
+showTime();
