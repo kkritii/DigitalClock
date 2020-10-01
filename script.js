1eebf39e62c1e93a0566d73e4e7b5c4425bf391d
kkritii <kritiipraz@gmail.com>
2020-10-01 13:57:12 +0545

    AM/PM error at 12 fixed

--- script.js
+++ script.js
@@ -9,31 +9,29 @@ function showTime(){
     var day = time.getDay();
     var dayList =["Sun","Mon","Tues","Wednes","Thurs","Fri","Satur"]; 
     day = dayList[day];
-    day = checkTime(day);
 
-    
+    day = checkTime(day);
 
     var h = time.getHours();
     var min = time.getMinutes();
     var sec = time.getSeconds();
     var period = "AM";
 
-    if (h == 0){
-        h = 12;
-    }
-    if ( h > 12){
-        h = h-12;
+    if ( h >= 12){
+        if (h >12 ){
+            h = h-12;
+        }    
         period = "PM";
     }
-
-    h = checkTime(h)
+    else if (h == 0){
+        h = 12;
+    }
+    
+    h = checkTime(h);
     m = checkTime(min);
     s = checkTime(sec);
 
     document.getElementById('date').innerHTML = day + "day, " + date + " " + month + " " + year;
-
-    
-
     document.getElementById('clock').innerHTML = h + ":" + m + ":" + s + " " + period;
     
     setTimeout('showTime()',1000);
@@ -45,4 +43,5 @@ function checkTime(i){
     }
     return i;
 }
+
 showTime();
